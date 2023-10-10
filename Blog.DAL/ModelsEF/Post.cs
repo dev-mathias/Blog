af47785ad1a075365b8e8f4c8a7ce6e9dbc8e596
de Walque Mathias <dewalquemathias@gmail.com>
2023-10-10 13:17:00 +0200

    Initialize database

---
+++ Blog.DAL/ModelsEF/Post.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Blog.Dal.ModelsEF
+{
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string? Titre { get; set; }
+        [Required]
+        public string? Contenu { get; set; }
+        [Required]
+        public DateTime DateDePublication { get; set; }
+        public string? ImageUrl { get; set; }
+
+        
+        [Required]
+        [ForeignKey("Admin")]
+        public int AdminId { get; set; }
+        public Admin? Admin { get; set; }
+
+        public ICollection<Comment>? Comments { get; set; }
+    }
+}
---
+++ Blog.DAL/ModelsEF/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blog.Dal.ModelsEF
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string? Lastname { get; set; }
+        [Required]
+        public string? FirstName { get; set; }
+        [Required]
+        public string? Nickname { get; set; }
+        [Required]
+        [EmailAddress]
+        public string? Email { get; set; }
+        [Required]
+        public string? Password { get; set; }
+        
+        public ICollection<Comment>? Comments { get; set; }
+    }
+}
